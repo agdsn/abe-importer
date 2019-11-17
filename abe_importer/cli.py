5b8973c8d7e40027438b38978dd2a076f4d4fe8a
Lukas Juhrich <lukas.juhrich@agdsn.de>
2019-11-17 03:16:51 +0100

    Add `-v` option for `logging.DEBUG`

--- abe_importer/cli.py
+++ abe_importer/cli.py
@@ -8,11 +8,14 @@ from abe_importer.session import create_session
 
 @click.command()
 @click.option('--abe-uri-file', default=".abe_uri")
-def main(abe_uri_file: str):
+@click.option('-v', '--verbose', is_flag=True,
+              help="Will raise the loglevel to DEBUG.")
+def main(abe_uri_file: str, verbose: True):
     abe_session = create_session(read_uri(uri_file=abe_uri_file))
     logger = logging.getLogger('abe-importer')
     log_format = "[%(levelname).4s] %(name)s:%(funcName)s:%(message)s"
-    logging.basicConfig(level=logging.INFO, format=log_format)
+    logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO,
+                        format=log_format)
     do_import(abe_session, logger)
 
 
