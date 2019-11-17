501b949964646ac8c5edbcc8a12f52248cb28191
Lukas Juhrich <lukas.juhrich@agdsn.de>
2019-11-17 07:01:32 +0100

    Add basic skeleton for new user import

--- abe_importer/importer/translations.py
+++ abe_importer/importer/translations.py
@@ -1,5 +1,7 @@
+from logging import Logger
 from typing import List
 
+from pycroft.helpers.i18n import deferred_gettext
 from pycroft.model import _all as pycroft_model
 
 from .. import model as abe_model
@@ -11,6 +13,10 @@ def add_sites(_, data: IntermediateData):
     site = pycroft_model.Site(name="Hochschulstraße")
     data.hss_site = site
     return [site]
+
+
+class ImportException(RuntimeError):
+    pass
 
 
 PycroftBase = pycroft_model.ModelBase
@@ -73,3 +79,79 @@ def translate_locations(ctx: Context, data: IntermediateData) -> List[PycroftBas
         data.access_rooms[access.id] = room
 
     return objs
+
+
+@reg.provides(pycroft_model.User)
+@reg.provides(pycroft_model.Account)
+@reg.provides(pycroft_model.UnixAccount)
+def translate_accounts(ctx: Context, data: IntermediateData) -> List[PycroftBase]:
+    objs = []
+    num_errors = 0
+    ctx.logger.info("There are %s accounts in total.",
+                    ctx.query(abe_model.Account).count())
+    # 1. Accounts which do _not_ have a pycroft mapping
+    accounts_with_access: List[abe_model.Account] = (
+        ctx.query(abe_model.Account)
+           .filter_by(pycroft_login=None)
+           .filter(abe_model.Account.access_id != None)
+           .all()
+    )
+    for acc in accounts_with_access:
+        try:
+            room = data.access_rooms[acc.access_id]
+        except KeyError as e:
+            ctx.logger.error("Could not find room for access %s (account %s)",
+                             acc.access_id, acc.account)
+            ctx.logger.debug(e.args)
+            num_errors += 1
+            continue
+
+        props: abe_model.AccountProperty = acc.property
+        chosen_login: str = acc.account
+        if '_' in chosen_login:
+            chosen_login = chosen_login.replace('_', '-')
+            ctx.logger.warning("Renaming '%s' to '%s'", acc.account, chosen_login)
+
+        # TODO find out whether this user already exists in pycroft
+
+        try:
+            user = pycroft_model.User(
+                login=chosen_login,
+                room=room,
+                address=room.address,
+                # birthdate=acc.birth_date,  # TODO add birth date to model
+                email=props.mail,
+            )
+        except pycroft_model.IllegalLoginError as e:
+            ctx.logger.error("Account '%s' has invalid login!",
+                             chosen_login)
+            ctx.logger.debug(e.args)
+            num_errors += 1
+            continue
+
+        # TODO find password from ldap (or import that later)
+
+        finance_account = pycroft_model.Account(
+            name=deferred_gettext(u"HSS:User {login}").format(id=chosen_login).to_json(),
+            type='USER_ASSET',
+        )
+        user.account = finance_account
+        unix_acc = pycroft_model.UnixAccount(home_directory=f"/home/{chosen_login}")
+        user.unix_account = unix_acc
+
+        # TODO add user to data
+
+        objs.extend([user, finance_account, unix_acc])
+
+        # TODO (in a separate translation) add memberships -> What about (former) orgs?.
+
+    # TODO import people with pycroft mapping
+    # TODO warn on people with neither access nor pycroft mapping
+    _maybe_abort(num_errors, ctx.logger)
+    return objs
+
+
+def _maybe_abort(num_errors: int, logger: Logger):
+    if num_errors:
+        logger.critical("Got %s errors. Aborting.", num_errors)
+        raise ImportException
