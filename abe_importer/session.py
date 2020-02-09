76e8805a92f2c4f63cd8db60b1e2a59f4cdbb47c
Lukas Juhrich <lukas.juhrich@agdsn.de>
2020-02-09 20:45:47 +0100

    Fix search_path in example connection string

--- abe_importer/session.py
+++ abe_importer/session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 
-# URL might be "postgres://postgres:password@172.22.0.2/abe?search_path=abe"
+# URL might be "postgres://postgres:password@172.22.0.2/abe?search_path=abe,public"
 
 def create_session(url):
     return Session(bind=create_engine(url))
