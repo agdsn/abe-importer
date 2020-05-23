3e639be8ce07b86c7f7ac94ffd1cd6a7d7d9ac0a
Lukas Juhrich <lukas.juhrich@agdsn.de>
2020-05-23 18:13:35 +0200

    Use connection_timeout

--- abe_importer/session.py
+++ abe_importer/session.py
@@ -5,4 +5,4 @@ from sqlalchemy.orm import Session
 # URL might be "postgres://postgres:password@172.22.0.2/abe?search_path=abe,public"
 
 def create_session(url):
-    return Session(bind=create_engine(url))
+    return Session(bind=(create_engine(url, connect_args={'connect_timeout': 10})))
